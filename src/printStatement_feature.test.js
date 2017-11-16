2302c5722934728db0786ff0cf1299d10a0d46b6
Tomás Casquero <tcasquero@abaenglish.com>
2017-11-16 17:16:41 +0100

    Add acceptance test

--- src/printStatement_feature.test.js
+++ src/printStatement_feature.test.js
@@ -5,7 +5,11 @@ describe('Acceptance criteria', () => {
   it('should given ammount console show acceptance result', () => {
     const account = new AccountService()
     account.deposit(1000)
-    expect(account.printStatement()).toContain(1000)
+    account.withdraw(100)
+    account.deposit(500)
+    expect(account.printStatement()).toContain("1000")
+    expect(account.printStatement()).toContain("-100")
+    expect(account.printStatement()).toContain("500")
   });
   
 });
